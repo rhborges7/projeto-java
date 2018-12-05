5f109cb75727454c61f8c8b8f5c7d71e1d26e1d9
rhborges7@gmail.com <rhborges7@gmail.com>
2018-12-05 19:17:00 -0200

    Infonote 08

---
+++ infonote_08/src/model/Funcionario.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Funcionario extends Usuario {
+
+
+	/**
+	 * @param matricula
+	 */
+	
+
+	private String matricula;
+
+	/**
+	 * @return the matricula
+	 */
+	public String getMatricula() {
+		return matricula;
+	}
+	
+
+	/**
+	 * 
+	 */
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	public Funcionario() {
+		super();
+		
+	}
+	
+	public Funcionario(String login, String senha, int tipo, String matricula) {
+		super(login, senha, tipo);
+		this.matricula = matricula;
+		// TODO Auto-generated constructor stub
+	}
+}
